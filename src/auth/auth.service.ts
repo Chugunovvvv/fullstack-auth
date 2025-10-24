710090dedfba22ece4b4013740fa9642894c9864
chugunov <cugunovroman03@gmail.com>
2025-10-23 23:31:49 -0700

    add auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,25 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { UserService } from '@/user/user.service';
 import { AuthMethod, User } from '__generated__';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
+import { verify } from 'argon2';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
-  public constructor(private userService: UserService) {}
+  public constructor(
+    private userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
 
-  public async register(dto: RegisterDto) {
+  public async register(req: Request, dto: RegisterDto) {
     const isExists = await this.userService.findByEmail(dto.email);
     if (isExists) {
       throw new ConflictException('User with this email already exists');
@@ -19,11 +32,48 @@ export class AuthService {
       AuthMethod.CREDENTIALS,
       false,
     );
-    console.log('Registered new user', newUser);
-    return this.saveSession(newUser);
+
+    return this.saveSession(req, newUser);
   }
 
-  public async saveSession(user: User) {
-    return console.log('Save session for user', user);
+  public async login(req: Request, dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email);
+    if (!user || !user.password) {
+      throw new NotFoundException('User with this email does not exist');
+    }
+    const isPasswordValid = await verify(user.password, dto.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    return this.saveSession(req, user);
+  }
+
+  public async logout(req: Request, res: Response): Promise<void> {
+    return new Promise((resolve, reject) => {
+      req.session.destroy((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException('Could not destroy session'),
+          );
+        }
+        res.clearCookie(this.configService.get('SESSION_NAME'));
+        resolve();
+      });
+    });
+  }
+
+  public async saveSession(req: Request, user: User) {
+    return new Promise((resolve, reject) => {
+      req.session.userId = user.id;
+      req.session.save((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException('Could not save session'),
+          );
+        }
+        resolve({ user });
+      });
+    });
   }
 }
